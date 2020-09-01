a655064f9c6ac956d7bd61bcca2f7d2f728f6a29
Meisam Taheri <60437951+Taheri-meisam@users.noreply.github.com>
2020-09-01 12:03:33 -0700

    Ive some changes

--- ZoomClass/ZoomClass.cpp
+++ ZoomClass/ZoomClass.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
+#include <string>
 
-int main()
-{
-    std::cout << "Hello World!\n";
-    std::cout << "Goodbye world";
+int age = 21; 
+bool sw = false;
+char Initial = 'm';
+std::string name = "Meisam";
+double pi = 3.14;
+
+
+int main() {
+	
+	std::cout << sw; 
+	std::cin >> age;
+	std::cout << "Your age is : " << age << "\n"; // "\n" = std::endl;   \t
+	std::cout << "You are young ";
+	std::cout << "s";
 }
+
+
+
